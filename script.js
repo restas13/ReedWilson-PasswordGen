d88f1d5e2f86a5519b5f80faf4234606ad6f4d1b
restas13 <72560360+restas13@users.noreply.github.com>
2023-03-13 18:22:05 -0700

    Added function that adds the numbers and other characters to the passworsContents array The code now prompts the user to choose a length for their password as well as to select what types of characters they would like in their password. NOTICE, the loop that generates the password is bugged and does not yet work in this verrsion of the code.

--- script.js
+++ script.js
@@ -5,10 +5,57 @@ var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var lower = "abcdefghijklmnopqrstuvwxyz".split('');
 var nums = "1234567890".split('');
 var special = "!@#$%^&*()_-+={[}]:;<,>.?/|".split('');
-console.log(special);
 
-function generatePassword(){
+function generatePassword() {
+  var length = parseInt(prompt("How long would you like your password to be? (must be greater than 7 and less than 128)"));
+  var passwordCharacters = [];
+  var finalPass = '';
 
+
+  if (length >= 8 && length <= 128) {
+    specifyPassword(length);
+  } else {
+    alert("Uh oh, it looks like you selected a number outside of the allowed range, please try again.");
+  }
+
+  for (var i; i < length; i++) {
+    console.log("working");
+  }
+
+  finalPass = passwordCharacters.toString();
+  console.log(passwordCharacters);
+  console.log("hello");
+
+}
+
+function specifyPassword(len) {
+  if (confirm("would you like upper case letters in your password?")) {
+    for (var i = 0; i < upper.length; i++) {
+      passwordContents.push(upper[i]);
+    }
+    console.log(passwordContents);
+  }
+
+  if (confirm("would you like lower case letters in your password?")) {
+    for (var i = 0; i < lower.length; i++) {
+      passwordContents.push(lower[i]);
+    }
+    console.log(passwordContents);
+  }
+
+  if (confirm("would you like nuumbers in your password?")) {
+    for (var i = 0; i < nums.length; i++) {
+      passwordContents.push(nums[i]);
+    }
+    console.log(passwordContents);
+  }
+
+  if (confirm("would you like special charcters in your password?")) {
+    for (var i = 0; i < special.length; i++) {
+      passwordContents.push(special[i]);
+    }
+    console.log(passwordContents);
+  }
 }
 
 // Get references to the #generate element
