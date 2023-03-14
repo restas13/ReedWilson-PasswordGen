50134041e238a88ed4fcf034d53955c2358a3d13
restas13 <72560360+restas13@users.noreply.github.com>
2023-03-13 19:42:56 -0700

    Final Changes and bug fix User now cannot choose to add no characters to their password and is prompted to restart if they fail to choose at least 1 character type to add. Comments have been added for readability.

--- script.js
+++ script.js
@@ -6,33 +6,43 @@ var lower = "abcdefghijklmnopqrstuvwxyz".split('');
 var nums = "1234567890".split('');
 var special = "!@#$%^&*()_-+={[}]:;<>.?/|".split('');
 
+//Generate password function, contains password making steps
 function generatePassword() {
   var length = parseInt(prompt("How long would you like your password to be? (must be greater than 7 and less than 128)"));
   var passwordCharacters = [];
   var finalPass = '';
 
-
+  //Checks if the length the user specified is within the set range
   if (length >= 8 && length <= 128) {
-    specifyPassword(length);
+    specifyPassword();
   } else {
     alert("Uh oh, it looks like you selected a number outside of the allowed range, please try again.");
     return "Your Secure Password";
   }
 
+  //Checks if the passwordContents array has any values to put into the password
+  if (passwordContents.length == 0) {
+    alert("Uh oh, it looks like you don't have any characters to use for your password, please try again and add at least one character type to make up the password.");
+    return "Your Secure Password";
+  }
+
+  //Loops for the ammount of characters the user wants and adds a random character
   for (var i = 0; i < parseInt(length); i++) {
     var num = Math.floor(Math.random() * passwordContents.length);
     passwordCharacters.push(passwordContents[num]);
   }
 
+  //Converts the password to a readable string
   finalPass = passwordCharacters.toString();
   finalPass = finalPass.replaceAll(/,/g, '');
   console.log(finalPass);
-  console.log("hello");
 
+  //returns the password value
   return finalPass;
 }
 
-function specifyPassword(len) {
+//Adds the users character types to the passwordContents array
+function specifyPassword() {
   if (confirm("would you like upper case letters in your password?")) {
     for (var i = 0; i < upper.length; i++) {
       passwordContents.push(upper[i]);
@@ -74,6 +84,7 @@ function writePassword() {
 
   passwordText.value = password;
 
+  //resets the passwordContents array
   passwordContents = [];
   console.log(passwordContents);
 
